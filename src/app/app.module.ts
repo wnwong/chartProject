919c90614be5de96ad1718edee28682040cee81b
timothy <timothy@bull-b.com>
2017-09-28 20:40:50 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { DxChartModule } from 'devextreme-angular';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+import {Service} from "../providers/chart-service/chart-service";
+import {ZoomingData} from "../providers/chart-service/chart-service";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +19,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    DxChartModule,
+    DxButtonModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +31,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ZoomingData,
+    Service
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ZoomingData, Service} from "../../providers/chart-service/chart-service";
+import {DxPieChartComponent} from "devextreme-angular";
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(DxPieChartComponent) chart: any;
+  zoomingData: ZoomingData[];
+  selectedRegion = "";
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public service: Service) {
+    this.zoomingData = service.getZoomingData();
   }
 
+  onInitialized(e: any) {
+    e.component.zoomArgument(100,200);
+  }
+
+  pointClick(e: any) {
+    var point = e.target;
+    point.showTooltip();
+    this.selectedRegion = point.argument;
+  }
+
+  customizeTooltip(arg: any) {
+    return {
+      text: arg.argumentText + "<br/>" + arg.valueText
+    };
+  }
 }
